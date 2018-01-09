acb6c5cfc9e1fb280f7705145f30abdaa36faf64
Byidi <contact@byidi.fr>
2018-01-09 15:58:48 +0100

    Amélioration popup produit

--- js/product.js
+++ js/product.js
@@ -3,42 +3,38 @@ function popupProducts(event, elem){
 
         var screenMidWidth = window.innerWidth/2;
         var screenMidHeight = window.innerHeight/2;
+        var elem2 =  elem.cloneNode(2);
+        elem2.setAttribute("id","produitPopup");
+        document.getElementsByTagName("body")[0].appendChild(elem2);
 
-        var elemMidWidth = elem.offsetWidth/2;
-        var elemMidHeight = elem.offsetHeight/2;
+        var elemMidWidth = elem2.offsetWidth/2;
+        var elemMidHeight = elem2.offsetHeight/2;
+        elem2.style.top = Number(screenMidHeight-elemMidHeight)+"px";
+        elem2.style.left = Number(screenMidWidth-elemMidWidth)+"px";
 
-        elem.style.position = "fixed";
-        elem.style.top = Number(screenMidHeight-elemMidHeight)+"px";
-        elem.style.left = Number(screenMidWidth-elemMidWidth)+"px";
-        elem.style.backgroundColor = "white";
-        elem.style.borderWidth = "5px";
-        elem.style.zIndex = "999";
-        elem.getElementsByClassName("description")[0].style.display = "block";
+        elem2.getElementsByClassName("addBasket")[0].onclick = function(e){
+            addToBasket(this.parentNode);
+            elem2.remove();
+            document.getElementById("backPopup").remove();
+        };
 
         var backPopup = document.createElement("div");
-        backPopup.style.position = "fixed";
-        backPopup.style.top = "0px";
-        backPopup.style.left = "0px";
-        backPopup.style.width = "100%";
-        backPopup.style.height = "100%";
-        backPopup.style.zIndex = "995";
-        backPopup.style.backgroundColor = "rgba(255,255,255,0.5)";
+        backPopup.setAttribute("id","backPopup");
+
         var body = document.getElementsByTagName("body")[0];
         body.appendChild(backPopup);
 
         backPopup.onclick = function(e){
-            elem.removeAttribute('style');
-            elem.onclick = function(e){popupProducts(e, this)};
-            elem.getElementsByClassName("description")[0].style.display = "none";
+            elem2.remove();
+            elem2.onclick = function(e){popupProducts(e, this)};
             backPopup.remove();
         };
 
-        elem.onclick = function(e){
+        elem2.onclick = function(e){
             if(!e.target.classList.contains("addBasket")){
-                elem.removeAttribute('style');
-                elem.getElementsByClassName("description")[0].style.display = "none";
+                elem2.remove();
                 backPopup.remove();
-                elem.onclick = function(e){popupProducts(e, this)};
+                elem2.onclick = function(e){popupProducts(e, this)};
             }
         };
     }
